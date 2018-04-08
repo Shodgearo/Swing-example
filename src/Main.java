8c4f6fc84866d5d2507123838b1008b0d34f5afd
Shodgearo <Shodkvest@gmail.com>
2018-04-08 21:48:53 +0600

    Adding in class initialization frame and minimum option for frame.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
 // Main class for feature width circles
+// Маленькая программка. По нажатию на кнопку мыши создаём кружки, которые сами двигаются
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        initFrame();
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(700, 550));
+        pack();
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
